c91bb03c5127d270197430dd2789d0ef11aff552
vratant11 <vratant2012061@akgec.ac.in>
2022-11-18 19:36:16 +0530

    AOS added

--- vite-project/src/Components/Homepage/Home.jsx
+++ vite-project/src/Components/Homepage/Home.jsx
@@ -12,43 +12,72 @@ const Home = () => {
         <button className="btn1">Make Your Profile</button>
         <button className="btn1">See Your Lists</button>
       </div>
-      <div style={{marginTop:"5px", padding:"15px", height:"25rem"}}>
-        <h1 style={{color:"#F1D112", marginBottom:"20px", marginLeft:"20px"}}>News</h1>
-      <Swiper/>
+      <div style={{ marginTop: "5px", padding: "15px", height: "25rem" }}>
+        <h1
+          style={{ color: "#F1D112", marginBottom: "20px", marginLeft: "20px" }}
+        >
+          News
+        </h1>
+        <Swiper />
       </div>
-      <a href="#" style={{textDecoration:"none"}}>
-      <div className="international">
-        <div className="photo"><img src="src\Images\globe.jpg" alt="Error" /></div>
-        <div className="text">
-          <h2>INTERNATIONAL SCHOLARSHIPS</h2>
-          <p style={{marginTop:"-20%"}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab
-            architecto voluptatem voluptates. Eius incidunt id rem voluptates
-            excepturi laboriosam sint veritatis, nesciunt ea aliquam, assumenda
-            aperiam perspiciatis esse voluptatum nemo non, blanditiis laudantium
-          </p>
+      <a href="#" style={{ textDecoration: "none" }}>
+        <div className="international">
+          <div
+            className="photo"
+            data-aos="fade-right"
+            data-aos-offset="500"
+            data-aos-easing="ease-in-sine"
+          >
+            <img src="src\Images\globe.jpg" alt="Error" />
+          </div>
+          <div className="text"
+          data-aos="fade-left"
+          data-aos-offset="500"
+          data-aos-easing="ease-in-sine"
+          >
+            <h2>INTERNATIONAL SCHOLARSHIPS</h2>
+            <p style={{ marginTop: "-20%" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              ab architecto voluptatem voluptates. Eius incidunt id rem
+              voluptates excepturi laboriosam sint veritatis, nesciunt ea
+              aliquam, assumenda aperiam perspiciatis esse voluptatum nemo non,
+              blanditiis laudantium
+            </p>
+          </div>
         </div>
-      </div>
       </a>
-      <a href="#" style={{textDecoration:"none"}}>
-
-      <div className="national">
-        <div className="text">
-          <h2>NATIONAL SCHOLARSHIPS</h2>
-          <p style={{marginTop:"-35%"}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab
-            architecto voluptatem voluptates. Eius incidunt id rem voluptates
-            excepturi laboriosam sint veritatis, nesciunt ea aliquam, assumenda
-            aperiam perspiciatis esse voluptatum nemo non, blanditiis laudantium
-          </p>
+      <a href="#" style={{ textDecoration: "none" }}>
+        <div className="national">
+          <div className="text"
+          data-aos="fade-right"
+          data-aos-offset="500"
+          data-aos-easing="ease-in-sine">
+            <h2>NATIONAL SCHOLARSHIPS</h2>
+            <p style={{ marginTop: "-35%" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              ab architecto voluptatem voluptates. Eius incidunt id rem
+              voluptates excepturi laboriosam sint veritatis, nesciunt ea
+              aliquam, assumenda aperiam perspiciatis esse voluptatum nemo non,
+              blanditiis laudantium
+            </p>
+          </div>
+          <div
+            className="photo"
+            data-aos="fade-left"
+            data-aos-offset="500"
+            data-aos-easing="ease-in-sine"
+            
+          >
+            <img
+              src="src\Images\119517630_108761140978167_8352676185718182111_n 1.png"
+              alt="Error"
+            />
+          </div>
         </div>
-        <div className="photo"><img src="src\Images\119517630_108761140978167_8352676185718182111_n 1.png" alt="Error" /></div>
-
-      </div>
       </a>
       <div className="footer">
         <div className="footerimg">
-      <img 
+          <img
             id="image"
             src="src\Images\Green Modern Education Online Course Logo (3) 1.png"
             alt="Error"
@@ -56,24 +85,23 @@ const Home = () => {
         </div>
         <div className="footertext">
           <div className="leftbox">
-            <ul >
+            <ul>
               <li>About Us</li>
               <li>Terms And Conditions</li>
             </ul>
           </div>
           <div className="middlebox">
-          <ul>
+            <ul>
               <li>Privacy Policy</li>
               <li>Hyperlink Policy</li>
             </ul>
           </div>
           <div className="rightbox">
-          <ul >
+            <ul>
               <li>Disclaimer</li>
               <li>Sitemap</li>
             </ul>
           </div>
-
         </div>
       </div>
     </>
